4db0150509476b2a68a682e98c7f0c0eabe292d9
Francois Polo <andasan@gmail.com>
2023-03-08 11:04:04 -0800

    added lab

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # React-101
+
+### Prerequisites
+```bash
+npm install
+```
+
+To run the lab:
+```bash
+npm run lab
+```
+
+Navigate to `http://localhost:1234`
--- no-cra-parcel/src/index.js
+++ no-cra-parcel/src/index.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-    render(){
-        return(
-            <h1>Hello World~</h1>
-        )
-    }
+const isAuth = true
+
+function Header(props) {
+    return <h1>Hello {props.username}</h1>
 }
 
+function Layout(props) {
+    console.log(props)
+    return (
+        <div style={{ backgroundColor: "royalblue" }}>
+            {props.children}
+        </div>
+    )
+}
+
+function Login(){
+    return <div>Login form here...</div>
+}
+
+function SignOut(){
+    return <button>Sign out</button>
+}
+
+//legacy class
+// class Header extends React.Component{
+//     customMethod(){
+//         console.log("Hello")
+//     }
+//     render(){
+
+//         return(
+//             <h1 onClick={this.customMethod}>Hello {this.props.username}</h1>
+//         )
+//     }
+// }
+
 const rootNode = document.getElementById('root')
-ReactDOM.render(<App />, rootNode)
+ReactDOM.render(
+    <Layout>
+        {isAuth ? <Header username="hoge" /> : <Login />}
+        {isAuth && <SignOut/>}
+    </Layout>,
+    rootNode)
