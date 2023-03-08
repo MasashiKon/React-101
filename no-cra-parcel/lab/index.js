f682e5bfde5713ee622e91ca2f678919092eb086
MasahiKon <konnonorth@gmail.com>
2023-03-08 11:29:14 -0800

    html to react

--- no-cra-parcel/lab/index.js
+++ no-cra-parcel/lab/index.js
@@ -4,7 +4,41 @@ import ReactDOM from 'react-dom'
 class App extends React.Component {
     render(){
         return(
-            <h1>Hello World~</h1>
+            <div>
+                <h1 style={{ textAlign: "center" }}>Hello World~</h1> 
+                <br />
+                <section>
+                    <Wrapper />
+                </section>
+            </div>
+        )
+    }
+}
+
+class Wrapper extends React.Component {
+    render(){
+        return(
+            <div className='wrapper'>
+                <Icon iconClass="fa fa-instagram" iconName="Instagram" />
+                <Icon iconClass="fa fa-facebook-f" iconName="Facebook" />
+                <Icon iconClass="fa fa-twitter" iconName="Twitter" />
+                <Icon iconClass="fa fa-linkedin" iconName="LinkedIn" />
+                <Icon iconClass="fa fa-youtube" iconName="YouTube" />
+                <Icon iconClass="fa fa-github" iconName="Github" />
+            </div>
+        )
+    }
+}
+
+class Icon extends React.Component {
+    render(){
+        return(
+            <div className="button">
+                <div className="icon">
+                    <i className={this.props.iconClass} ></i>
+                </div>
+                <span>{this.props.iconName}</span>
+            </div>
         )
     }
 }
